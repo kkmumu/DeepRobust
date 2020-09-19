bf8604614898f682307665bec601d7ef40c86eba
Shangwen Sun <ssw@pku.edu.cn>
2020-09-19 15:08:51 +0800

    Update train_model.py

--- deeprobust/image/netmodels/train_model.py
+++ deeprobust/image/netmodels/train_model.py
@@ -13,7 +13,7 @@ from torchvision import datasets, transforms
 import numpy as np
 from PIL import Image
 
-def train(model, data, device, maxepoch, data_path = './', save_per_epoch = 10, seed = 100, shuffle = False, random_train = False):
+def train(model, data, device, maxepoch, ensemble_size, t, data_path = './', save_per_epoch = 10, seed = 199, seedin = 100, shuffle = False):
     """train.
 
     Parameters
@@ -41,14 +41,14 @@ def train(model, data, device, maxepoch, data_path = './', save_per_epoch = 10, 
     """
 
     torch.manual_seed(seed)
-
-    train_loader, test_loader = feed_dataset(data, data_path, random_train)
+    import deeprobust.image.netmodels.feed_dataset as fd
+    train_loader, test_loader = fd.feed_dataset(data, data_path, seedin, random_train = True)
     device = torch.device("cuda")
 
     if (model == 'CNN'):
         import deeprobust.image.netmodels.CNN as MODEL
         #from deeprobust.image.netmodels.CNN import Net
-        train_net = MODEL.Net().to(device = device)
+        train_net = MODEL.Net(ensemble_size = ensemble_size, t = t).to(device = device)
 
     elif (model == 'ResNet18'):
         import deeprobust.image.netmodels.resnet as MODEL
@@ -113,71 +113,4 @@ def train(model, data, device, maxepoch, data_path = './', save_per_epoch = 10, 
     print("========Generalization Error========")
     print('Generalization Error: {:.4f}\n'.format(generalization_error))
 
-
-def feed_dataset(data, data_dict, random_train = False):
-    if random_train == True:
-        if(data == 'MNIST'):
-            train_set = datasets.MNIST('./', train=True, download = True,transform=transforms.Compose([transforms.ToTensor()]))
-            test_set = datasets.MNIST('../data', train=False, download = True,transform=transforms.Compose([transforms.ToTensor()]))
-            full_set = torch.utils.data.ConcatDataset([train_set,test_set])
-            
-            trans = transforms.Compose(transforms = [
-                transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
-            
-            train_len = 60000
-            test_len = 10000
-            trainset_new, testset_new = torch.utils.data.random_split(full_set,[train_len, test_len])
-            trainset_new.transform = trans
-            testset_new.transform = trans
-            train_loader = torch.utils.data.DataLoader(trainset_new, batch_size = 64, shuffle = True)
-            test_loader = torch.utils.data.DataLoader(testset_new, batch_size = 1000, shuffle = True)
-            
-        else:
-            pass
-        
-        return train_loader, test_loader
-    
-    else:
-        if(data == 'CIFAR10'):
-            transform_train = transforms.Compose([
-                    transforms.RandomCrop(32, padding=5),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-                    ])
-        
-            transform_val = transforms.Compose([
-                    transforms.ToTensor(),
-                    #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-                    ])
-        
-            train_loader = torch.utils.data.DataLoader(
-                     datasets.CIFAR10(data_dict, train=True, download = True,
-                            transform=transform_train),
-                     batch_size= 1000, shuffle=True) #, **kwargs)
-        
-            test_loader  = torch.utils.data.DataLoader(
-                     datasets.CIFAR10(data_dict, train=False, download = True,
-                            transform=transform_val),
-                    batch_size= 1000, shuffle=True) #, **kwargs)
-        
-        elif(data == 'MNIST'):
-            train_loader = torch.utils.data.DataLoader(
-                     datasets.MNIST(data_dict, train=True, download = True,
-                     transform=transforms.Compose([transforms.ToTensor(),
-                     transforms.Normalize((0.1307,), (0.3081,))])),
-                     batch_size=64,
-                     shuffle=True)
-        
-            test_loader = torch.utils.data.DataLoader(
-                    datasets.MNIST('../data', train=False, download = True,
-                    transform=transforms.Compose([transforms.ToTensor(),
-                    transforms.Normalize((0.1307,), (0.3081,))])),
-                    batch_size=1000,
-                    shuffle=True)
-        
-        elif(data == 'ImageNet'):
-            pass
-        
-        return train_loader, test_loader
-
+    return generalization_error
